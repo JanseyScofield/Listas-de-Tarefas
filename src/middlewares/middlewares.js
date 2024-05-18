31517d008531c69ad11c8d3eaec173b60a19cabb
Jansey Scofield da Silva Filho <janscofield10@gmail.com.>
2024-05-17 22:51:18 -0300

    Adicionando regras de negócio

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -1,7 +1,15 @@
 function checkNome(req,res, next){
-    if(!req.body.nome) 
-        return res.status(400).send({message: "É necessário enviar um nome."})
+    if(req.body.nome.length < 3) 
+        return res.status(400).send({message: "É necessário enviar um nome com mais de 3 caracteres."})
     return next()
 }
 
-module.exports  = {checkNome}
+function checkIdade(req,res,next){
+    var dataNascimento = new Date(req.body.dataNascimento)
+    var dataMinima = new Date('2014-05-18')
+    if(dataNascimento > dataMinima)
+        return res.status(400).send({message : "Um responsável não pode ter menos de 10 anos"})
+    return  next()
+}
+
+module.exports  = {checkNome,checkIdade}
--- src/routes/responsaveis.js
+++ src/routes/responsaveis.js
@@ -1,9 +1,10 @@
 const express  = require("express")
 const controllers = require("../controllers/responsaveis")
+const middlewares = require("../middlewares/middlewares")
 const router  =  express.Router()
 
 router.get("/", controllers.list)
-router.post("/",controllers.create)
-router.put("/:id",controllers.update)
+router.post("/", middlewares.checkNome, middlewares.checkIdade,controllers.create)
+router.put("/:id",middlewares.checkNome, middlewares.checkIdade, controllers.update)
 router.delete("/:id", controllers.remove)
 module.exports = router
